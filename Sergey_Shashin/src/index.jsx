bd0324effefe5b8dc9c089069362311de5412959
SergeyShashin <s9997910@yandex.ru>
2019-12-06 15:52:07 +0300

    dz1

--- Sergey_Shashin/src/index.jsx
+++ Sergey_Shashin/src/index.jsx
@@ -1,20 +1,37 @@
-import 'assets/global.scss';
-
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-import { Router } from 'components/Router/Router';
 
-class Container extends React.Component {
+class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      text: [],
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+
+    this.setState({
+      text: this.state.text.concat('Нормально')
+    });
   }
 
   render() {
-    return <div><Router /></div>
+    return (
+      <div>
+        <button onClick={this.handleClick}>Отправить</button>
+        {this.state.text.map(e => (<div>{e}</div>))}
+      </div>
+    )
   }
+
 }
 
+
 ReactDom.render(
-  <Container />,
+  <App />,
   document.getElementById('root'),
 );
--- Sergey_Shashin/webpack.config.js
+++ Sergey_Shashin/webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
       components: path.resolve(__dirname, 'src', 'components'),
       assets: path.resolve(__dirname, 'src', 'assets'),
       pages: path.resolve(__dirname, 'src', 'pages'),
+      reducers: path.resolve(__dirname, 'src', 'reducers'),
     }
   },
   module: {
